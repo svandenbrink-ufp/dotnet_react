116706e1c554527525ad08a6431351273d115dcc
svandenbrink-ufp <steve.vandenbrink@ufpi.com>
2024-10-18 15:07:42 -0400

    Added material ui (mui)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
-createRoot(document.getElementById('root')).render(
+const theme = createTheme({});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
